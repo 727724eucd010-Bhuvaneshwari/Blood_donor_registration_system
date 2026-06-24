70ccdb65380031feef01be7bf08ff7de294a89ba
Bhuvaneshwari <kbhuvaneshwari757@gmail.com>
2026-06-24 23:43:33 +0530

    Initial Spring Boot setup

---
+++ blood-donor-system/src/main/java/com/blooddonor/blooddonorsystem/BloodDonorSystemApplication.java
@@ -0,0 +1,13 @@
+package com.blooddonor.blooddonorsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonorSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodDonorSystemApplication.class, args);
+	}
+
+}
---
+++ blood-donor-system/src/test/java/com/blooddonor/blooddonorsystem/BloodDonorSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blooddonor.blooddonorsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonorSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
