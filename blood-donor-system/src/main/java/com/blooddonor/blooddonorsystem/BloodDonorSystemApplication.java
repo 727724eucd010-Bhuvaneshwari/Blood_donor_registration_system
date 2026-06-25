0f25ae2f2e33b529652bf3dea6ca4c149824afd7
Bhuvaneshwari <kbhuvaneshwari757@gmail.com>
2026-06-25 07:00:06 +0530

    This is my code

--- blood-donor-system/src/main/java/com/blooddonor/blooddonorsystem/BloodDonorSystemApplication.java
+++ blood-donor-system/src/main/java/com/blooddonor/blooddonorsystem/BloodDonorSystemApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BloodDonorSystemApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Blood Donor System Application is starting...");
 		SpringApplication.run(BloodDonorSystemApplication.class, args);
 	}
 
